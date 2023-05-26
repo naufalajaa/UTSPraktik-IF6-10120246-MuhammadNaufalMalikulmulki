3d4b88953f7c8a493f8aed8457faf3efb61b90dc
naufalajaa <naufalajaa475@gmail.com>
2023-05-26 18:08:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/utspraktik_if6_10120246_muhammadnaufalmalikulmulki/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.utspraktik_if6_10120246_muhammadnaufalmalikulmulki
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
